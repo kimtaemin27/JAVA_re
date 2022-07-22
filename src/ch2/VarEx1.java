aa19cc730c66656755e8ac0ceab0532ab369b7ea
김태민 <ktm3628@naver.com>
2022-07-22 17:41:22 +0900

    07-22

---
+++ src/ch2/VarEx1.java
@@ -0,0 +1,22 @@
+package ch2;
+
+public class VarEx1 {
+	public static void main(String [] args) {
+		
+		int year = 0;
+		int age = 14;
+		
+		System.out.println(year);
+		System.out.println(age);
+		
+		// 변수 age의 값에 2000을 더해서 변수 year에 저장
+		year = age + 2000;
+		// 변수 age에 저장된 값을 1 증가시킨다
+		age = age+1;
+		
+		System.out.println(year);
+		System.out.println(age);
+		
+	}
+
+}
