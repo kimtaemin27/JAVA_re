03119e586f7de60a2c4dbb75fa3a8125ed446303
김태민 <ktm3628@naver.com>
2022-07-23 17:16:04 +0900

    07-23

---
+++ src/ch2/OperatorEx5.java
@@ -0,0 +1,13 @@
+package ch2;
+
+public class OperatorEx5 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(-10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%-8);
+
+	}
+
+}
