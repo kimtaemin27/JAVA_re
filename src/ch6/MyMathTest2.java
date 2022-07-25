a7397538d1a7867b4748670140a738ca6fe7b01f
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2022-07-25 21:15:17 +0900

    07-25

---
+++ src/ch6/MyMathTest2.java
@@ -0,0 +1,62 @@
+package ch6;
+
+class MyMath2 {
+	long a;
+	long b;
+	
+	// 인스턴스 변수 a, b 만을 이용해서 작업 함으로 매개변수가 필요 없다
+	long add() {
+		return a+b;
+	}
+	
+	long subtract() {
+		return a-b;
+	}
+	
+	long multiply() {
+		return a*b;
+	}
+	
+	double divide() {
+		return a/b;
+	}
+	
+	// 인스턴스 변수와 관계없이 매개변수 만으로 작업이 가능하다
+	static long add(long a, long b) {
+		return a + b;
+	}
+	static long subtract(long a, long b) {
+		return a-b;
+	}
+	
+	static long multiply(long a, long b) {
+		return a*b;
+	}
+	
+	static double divide(double a, double b) {
+		return a/b;
+	}
+}
+
+public class MyMathTest2 {
+
+	public static void main(String[] args) {
+		
+		// 클래스 메서드 호출, 인스턴스 생성없이 호출 가능
+		System.out.println(MyMath2.add(200L, 100L));
+		System.out.println(MyMath2.subtract(200L, 100L));
+		System.out.println(MyMath2.multiply(200L, 100L));
+		System.out.println(MyMath2.divide(200.0, 100.0));
+		
+		// 인스턴스 생성
+		MyMath2 mm = new MyMath2();
+		mm.a = 200L;
+		mm.b = 100L;
+		// 인스턴스 메서드는 객체 생성 후 호출 가능
+		System.out.println(mm.add());
+		System.out.println(mm.subtract());
+		System.out.println(mm.multiply());
+		System.out.println(mm.divide());
+	}
+
+}
