e57806721ebb94634e1fbe7bbb14decf8ba61e21
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2022-07-26 18:18:37 +0900

    07-26

---
+++ src/ch7/PackageTest.java
@@ -0,0 +1,12 @@
+package ch7;
+// package com.codechobo.book;
+// 새로운 패키지 생성 후 이동 가능
+
+public class PackageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
