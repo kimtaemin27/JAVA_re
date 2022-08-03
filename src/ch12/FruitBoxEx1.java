4e2994abf51a557f015415149bf74c613e7c694a
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2022-08-03 13:26:43 +0900

    08-03

---
+++ src/ch12/FruitBoxEx1.java
@@ -0,0 +1,78 @@
+package ch12;
+
+import java.util.ArrayList;
+
+class Fruit {
+	@Override
+	public String toString() {
+		return "Fruit";
+	}
+}
+
+class Apple extends Fruit {
+	@Override
+	public String toString() {
+		return "Apple";
+	}
+}
+
+class Grape extends Fruit {
+	@Override
+	public String toString() {
+		return "Grape";
+	}
+}
+
+class Toy {
+	@Override
+	public String toString() {
+		return "Toy";
+	}
+}
+
+public class FruitBoxEx1 {
+
+	public static void main(String[] args) {
+		
+		Box<Fruit> fruitBox = new Box<Fruit>();
+		Box<Apple> appleBox = new Box<Apple>();
+		Box<Grape> grapeBox = new Box<Grape>();
+		Box<Toy> toyBox = new Box<Toy>();
+		
+		fruitBox.add(new Fruit());
+		fruitBox.add(new Apple());
+		
+		appleBox.add(new Apple());
+		appleBox.add(new Apple());
+		
+		toyBox.add(new Toy());
+		
+		System.out.println(fruitBox);
+		System.out.println(appleBox);
+		System.out.println(toyBox);
+
+	}
+
+}
+
+class Box<T> {
+	
+	ArrayList<T> list = new ArrayList<T>();
+	
+	void add(T item) {
+		list.add(item);
+	}
+	
+	T get(int i) {
+		return list.get(i);
+	}
+	
+	int size() {
+		return list.size();
+	}
+	
+	@Override
+	public String toString() {
+		return list.toString();
+	}
+}
