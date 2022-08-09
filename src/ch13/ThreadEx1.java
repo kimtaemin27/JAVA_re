ce20bf838bc3df4a1386254678b855d294e5787e
김태민 <103544589+kimtaemin27@users.noreply.github.com>
2022-08-09 20:34:48 +0900

    08-09

---
+++ src/ch13/ThreadEx1.java
@@ -0,0 +1,35 @@
+package ch13;
+
+public class ThreadEx1 {
+
+	public static void main(String[] args) {
+		
+		ThreadEx1_1 t1 = new ThreadEx1_1();
+		
+		Runnable r = new ThreadEx1_2();
+		Thread t2 = new Thread(r);
+		
+		t1.start();
+		t2.start();
+
+	}
+
+}
+
+class ThreadEx1_1 extends Thread {
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			// 조상인 Thread 의 getName() 을 호출
+			System.out.println(getName());
+		}
+	}
+}
+
+class ThreadEx1_2 implements Runnable {
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			// Thread.currentThread() - 현재 실행중인 Thread 를 반환한다
+			System.out.println(Thread.currentThread().getName());
+		}
+	}
+}
